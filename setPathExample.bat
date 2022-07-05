7f25ec7ca611b18826773d5c4cea401e71da581a
Huy Gia Nguyen <90328373+hugn456@users.noreply.github.com>
2022-07-05 18:30:10 +1200

    Update setPathExample.bat

--- setPathExample.bat
+++ setPathExample.bat
@@ -5,7 +5,7 @@
 @set PATH=C:\Users\hugn456\Downloads\python-3.10.5-embed-amd64;%PATH%
 
 :: set the path for the pip which was manually installed
-::@set PATH=C:\Users\hugn456\Downloads\python-3.10.5-embed-amd64\Scripts;%PATH%
+@set PATH=C:\Users\hugn456\Downloads\python-3.10.5-embed-amd64\Scripts;%PATH%
 
 :: save the property of this file as cmd.exe
 @cmd.exe /K
